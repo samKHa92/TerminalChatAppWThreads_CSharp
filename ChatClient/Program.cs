6025fe5a627bcf3cd4c134171040a38f08e1f27d
samKHa92 <samkha@gfmstudio.com>
2022-07-20 21:50:09 +0400

    initial commit

---
+++ ChatClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ChatClient
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect(IPAddress.Parse("127.0.0.1"), 5003);
+            Console.WriteLine("You have successfully connected to the server!");
+            NetworkStream ns = client.GetStream();
+            Thread th = new Thread(o => RecieveData((TcpClient)o));
+            th.Start(client);
+
+            string inp = string.Empty;
+            while (inp != "Exit")
+            {
+                inp = Console.ReadLine();
+                byte[] BufferStream = Encoding.ASCII.GetBytes(inp);
+                ns.Write(BufferStream, 0, BufferStream.Length);
+            }
+
+            client.Client.Shutdown(SocketShutdown.Send);
+            th.Join();
+            ns.Close();
+            client.Close();
+            Console.WriteLine("You have disconnected from the server!");
+
+        }
+
+        public static void RecieveData(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            byte[] ReceivedBytes = new byte[2048];
+            int ReceivedBytesLengh;
+
+            while ((ReceivedBytesLengh = ns.Read(ReceivedBytes, 0, ReceivedBytes.Length)) > 0)
+            {
+                Console.Write(Encoding.ASCII.GetString(ReceivedBytes, 0, ReceivedBytesLengh));
+            }
+        }
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static readonly Dictionary<int, TcpClient> clients = new Dictionary<int, TcpClient>();
+        static readonly object _lock = new object();
+        public static void Main(string[] args)
+        {
+            int IdCounter = 1;
+            TcpListener ServerSocket = new TcpListener(IPAddress.Any, 5003);
+            ServerSocket.Start();
+
+            while (true)
+            {
+                TcpClient client = ServerSocket.AcceptTcpClient();
+                lock (_lock) clients.Add(IdCounter, client);
+                Console.WriteLine("Client " + IdCounter + " Joined");
+
+                Thread th = new Thread(HandleClients);
+                th.Start(IdCounter);
+                IdCounter++;
+            }
+        }
+
+        public static void HandleClients(object o)
+        {
+            int ClientId = (int)o;
+            TcpClient client;
+
+            lock (_lock) client = clients[ClientId];
+
+            while(true)
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] BufferStream = new byte[2048];
+                int BufferStreamLength = stream.Read(BufferStream, 0, BufferStream.Length);
+
+                if(BufferStreamLength == 0)
+                {
+                    break;
+                }
+                string data = Encoding.ASCII.GetString(BufferStream, 0, BufferStreamLength);
+                HandleBroadcast(data);
+                Console.WriteLine(data);
+            }
+
+
+        }
+
+        public static void HandleBroadcast(string data)
+        {
+            byte[] BufferStream = Encoding.ASCII.GetBytes(data + Environment.NewLine);
+            lock (_lock)
+            {
+                foreach (TcpClient tcpclient in clients.Values)
+                {
+                    NetworkStream stream = tcpclient.GetStream();
+                    stream.Write(BufferStream, 0, BufferStream.Length);
+                }
+            }
+        }
+    }
+}
